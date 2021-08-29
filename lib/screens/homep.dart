92c482c1a25f83050d1c8c098d0016e9ab113315
yassin800 <57330560+yassin800@users.noreply.github.com>
2021-08-29 06:13:36 +0300

    query

--- lib/screens/homep.dart
+++ lib/screens/homep.dart
@@ -1,160 +1,194 @@
- import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
+import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:wakelni/constants/constants.dart';
 import 'package:wakelni/widgets/ourbar.dart';
+
 class homep extends StatelessWidget {
-  const homep({ Key? key }) : super(key: key);
-  
+  const homep({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-     Widget image_slider_carousel=Container(
+    Widget image_slider_carousel = Container(
       height: 200,
       child: Carousel(
-         dotBgColor: Colors.transparent,
-        images: [Image.asset("assets/1.png",fit: BoxFit.cover,),
-                 Image.asset("assets/1.png",fit: BoxFit.cover,),
-                 Image.asset("assets/1.png",fit: BoxFit.cover,),
-                 Image.asset("assets/1.png",fit: BoxFit.cover,),],
+        dotBgColor: Colors.transparent,
+        images: [
+          Image.asset(
+            "assets/1.png",
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            "assets/1.png",
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            "assets/1.png",
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            "assets/1.png",
+            fit: BoxFit.cover,
+          ),
+        ],
         indicatorBgPadding: 5.0,
         autoplay: false,
-
       ),
-      
     );
-    return ListView(children: [
-         Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,  
-             children: [
-             InkWell(
-                 onTap: (){
-                   showDialog(context: context,
-                     builder: (BuildContext context)
-                     {
-                       return OurBar();
-                     }
-                     
-                     );
-                 },
-                 child: Icon(Icons.menu,color: Colors.white,)),
-             Image.asset("assets/3.png",width: 200,),
-             InkWell(
-               onTap:(){
-                 Navigator.of(context).pushNamed('/notificatios');
-               },
-                 child: Icon(Icons.notifications_none_outlined,color: Colors.white, ))]
-             ),
-      Container(
-        child: image_slider_carousel ,),
-             SizedBox(height: 20,),
-             Padding(
-               padding: const EdgeInsets.all(16.0),
-               child: Text("Categories",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
-             ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Stack(children: [
-                    Column(
-                    children: [
-                      SizedBox(height: size.height * 0.05,),
-                      ClipRRect(borderRadius: BorderRadius.circular(20),
-                        child:
-                        Container(
-                          color: Colors.white,
-                          width: size.width * 0.42,
-                          height: size.height * 0.1,
-                        ),
+    return ListView(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          InkWell(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return OurBar();
+                    });
+              },
+              child: Icon(
+                Icons.menu,
+                color: Colors.white,
+              )),
+          Image.asset(
+            "assets/3.png",
+            width: 200,
+          ),
+          InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/notificatios');
+              },
+              child: Icon(
+                Icons.notifications_none_outlined,
+                color: Colors.white,
+              ))
+        ]),
+        Container(
+          child: image_slider_carousel,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            "Categories",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        color: Colors.white,
+                        width: size.width * 0.42,
+                        height: size.height * 0.1,
                       ),
-                    ],
-                  ),
-                   Positioned(
-                        bottom: size.height * 0.045,
-                        left: size.width * 0.09,
-                        child:
-                        SvgPicture.asset("assets/Repeat Grid 19.svg")
-                   ),
-                  ],
-                  ),
-                  Stack(children: [
-                    Column(
-                      children: [
-                        SizedBox(height: size.height * 0.05,),
-                        ClipRRect(borderRadius: BorderRadius.circular(20),
-                          child:
-                          Container(
-                            color: Colors.white,
-                            width: size.width * 0.42,
-                            height: size.height * 0.1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                        bottom: size.height * 0.045,
-                        left: size.width * 0.09,
-                        child:
-                        SvgPicture.asset("assets/Repeat Grid 22.svg")
                     ),
                   ],
-                  ),
-                ],
-              ),
-              SizedBox(height: size.height * 0.01,),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Stack(children: [
-                    Column(
-                      children: [
-                        SizedBox(height: size.height * 0.05,),
-                        ClipRRect(borderRadius: BorderRadius.circular(20),
-                          child:
-                          Container(
-                            color: Colors.white,
-                            width: size.width * 0.42,
-                            height: size.height * 0.1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                        bottom: size.height * 0.045,
-                        left: size.width * 0.09,
-                        child:
-                        SvgPicture.asset("assets/des.svg"),
-                    ),
-                  ],
-                  ),
-                  Stack(children: [
-                    Column(
-                      children: [
-                        SizedBox(height: size.height * 0.05,),
-                        ClipRRect(borderRadius: BorderRadius.circular(20),
-                          child:
-                          Container(
-                            color: Colors.white,
-                            width: size.width * 0.42,
-                            height: size.height * 0.1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                        bottom: size.height * 0.045,
-                        left: size.width * 0.09,
-                        child:
-                        SvgPicture.asset("assets/Repeat Grid 21.svg")
-                    ),
-                  ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: size.height * 0.1,
-              ),
-              
+                ),
+                Positioned(
+                    bottom: size.height * 0.045,
+                    left: size.width * 0.09,
+                    child: SvgPicture.asset("assets/Repeat Grid 19.svg")),
               ],
+            ),
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        color: Colors.white,
+                        width: size.width * 0.42,
+                        height: size.height * 0.1,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                    bottom: size.height * 0.045,
+                    left: size.width * 0.09,
+                    child: SvgPicture.asset("assets/Repeat Grid 22.svg")),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: size.height * 0.01,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        color: Colors.white,
+                        width: size.width * 0.42,
+                        height: size.height * 0.1,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: size.height * 0.045,
+                  left: size.width * 0.09,
+                  child: SvgPicture.asset("assets/des.svg"),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        color: Colors.white,
+                        width: size.width * 0.42,
+                        height: size.height * 0.1,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                    bottom: size.height * 0.045,
+                    left: size.width * 0.09,
+                    child: SvgPicture.asset("assets/Repeat Grid 21.svg")),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: size.height * 0.1,
+        ),
+      ],
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,111 +10,102 @@ class LoginScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
         child: Scaffold(
-          backgroundColor: PrimaryColor,
-          body: ListView(
-            children: [
-              SizedBox(
-                height: size.height * 0.08 ,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/logo1.png'),
-                ],
-              ),
-              SizedBox(
-                height: size.height *0.06,
-              ),
-              Stack(
-                children:[
-                  Center(
-                    child:Image.asset('assets/BG.png'),
-                  ),
-                  Positioned(
-                    left: size.width *0.35,
-                      top: size.height * 0.03,
-                      child: Text("تسجيل دخول",style: TextStyle(
-                      fontSize: 25.0
-                      ),
-                      ),
-                  ),
-                  Positioned(
-                    top: size.height * 0.08,
-                      right: size.width *0.1,
-                      left: size.width * 0.1,
-                      child: TextContainer(
-                    labtext: "البريد الالكتروني",
-                    onchange: (){},
-
-                  ),
-                  ),
-                  Positioned(
-                      top: size.height * 0.18,
-                      right: size.width *0.1,
-                      left: size.width * 0.1,
-                      child: TextContainer(
-                        labtext: "كلمة السر",
-                        onchange: (){},
-                      ),
-                  ),
-                  Positioned(
-                    top: size.height * 0.28,
-                    left: size.width * 0.1,
-                    child: InkWell(
-                      onTap:(){
-                        Navigator.of(context).pushNamed('/forgetpass');
-                      },
-                      child: Text("نسيت كلمة السر؟",style: TextStyle(
-                        color: Colors.blue[900],
-                      ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: size.height * 0.33,
-                    left: size.width * 0.75,
-                    child: InkWell(
-                      onTap: (){},
-                        child: SvgPicture.asset("assets/Button.svg")),
-                  ),
-                ],
-              ),
-              SvgPicture.asset('assets/Mobile login-rafiki.svg'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:[ SignInButton(
-
-                  Buttons.Facebook,
-                  shape: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-                ]
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[ SignInButton(
-
-                    Buttons.Google,
-                    shape: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.transparent
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    onPressed: () {},
-                  ),
-                  ]
-              )
-
-       ]
+      backgroundColor: PrimaryColor,
+      body: ListView(children: [
+        SizedBox(
+          height: size.height * 0.08,
         ),
-    )
-    );
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo1.png'),
+          ],
+        ),
+        SizedBox(
+          height: size.height * 0.06,
+        ),
+        Stack(
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/BG.png',
+                width: size.width * 0.9,
+                height: size.height * 0.45,
+              ),
+            ),
+            Positioned.fill(
+              left: size.width * 0.35,
+              top: size.height * 0.03,
+              child: Text(
+                "تسجيل دخول",
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.08,
+              right: size.width * 0.1,
+              left: size.width * 0.1,
+              child: TextContainer(
+                labtext: "البريد الالكتروني",
+                onchange: () {},
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.18,
+              right: size.width * 0.1,
+              left: size.width * 0.1,
+              child: TextContainer(
+                labtext: "كلمة السر",
+                onchange: () {},
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.28,
+              left: size.width * 0.1,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/forgetpass');
+                },
+                child: Text(
+                  "نسيت كلمة السر؟",
+                  style: TextStyle(
+                    color: Colors.blue[900],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.33,
+              left: size.width * 0.75,
+              child: InkWell(
+                  onTap: () {}, child: SvgPicture.asset("assets/Button.svg")),
+            ),
+          ],
+        ),
+        SvgPicture.asset('assets/Mobile login-rafiki.svg'),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SignInButton(
+            Buttons.Facebook,
+            shape: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ]),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SignInButton(
+            Buttons.Google,
+            shape: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onPressed: () {},
+          ),
+        ])
+      ]),
+    ));
   }
 }
-
