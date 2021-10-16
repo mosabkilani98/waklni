599c8c9a4667200a77c6fdd56ac17fc25386fc63
yassin800 <57330560+yassin800@users.noreply.github.com>
2021-10-16 14:44:50 +0300

    log

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
 import 'package:wakelni/constants/constants.dart';
-import 'package:wakelni/provide/google.dart';
-import 'package:wakelni/widgets/textfield.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
-
+import 'package:wakelni/widgets/loginscreen.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,47 +8,7 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       backgroundColor: PrimaryColor,
-      body: ListView(children: [
-        SizedBox(
-          height: size.height * 0.08,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/logo1.png'),
-          ],
-        ),
-        SizedBox(
-          height: size.height * 0.06,
-        ),
-       LoginScreen(),
-        SvgPicture.asset('assets/Mobile login-rafiki.svg'),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SignInButton(
-            Buttons.Facebook,
-            shape: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(
-                color: Colors.transparent,
-              ),
-            ),
-            onPressed: () {},
-          ),
-        ]),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SignInButton(
-            Buttons.Google,
-            shape: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            onPressed: () {
-              final provider=Provider.of<GoogleSignInProvider>(context,listen: false);
-              provider.googlelogin();
-            },
-          ),
-        ])
-      ]),
+      body: LoginWidget(),
     ));
   }
 }
--- lib/widgets/loginscreen.dart
+++ lib/widgets/loginscreen.dart
@@ -1,70 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+import 'package:wakelni/provide/google.dart';
 import 'package:wakelni/widgets/textfield.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Center(
-          child: Image.asset(
-            'assets/BG.png',
-            width: size.width * 0.9,
-            height: size.height * 0.45,
+    return ListView(children: [
+      SizedBox(
+        height: size.height * 0.08,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/logo1.png'),
+        ],
+      ),
+      SizedBox(
+        height: size.height * 0.06,
+      ),
+      Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/BG.png',
+              width: size.width * 0.9,
+              height: size.height * 0.45,
+            ),
           ),
-        ),
-        Positioned.fill(
-          left: size.width * 0.35,
-          top: size.height * 0.03,
-          child: Text(
-            "تسجيل دخول",
-            style: TextStyle(fontSize: 25.0),
-          ),
-        ),
-        Positioned(
-          top: size.height * 0.08,
-          right: size.width * 0.1,
-          left: size.width * 0.1,
-          child: TextContainer(
-            labtext: "البريد الالكتروني",
-            onchange: () {},
-          ),
-        ),
-        Positioned(
-          top: size.height * 0.18,
-          right: size.width * 0.1,
-          left: size.width * 0.1,
-          child: TextContainer(
-            labtext: "كلمة السر",
-            onchange: () {},
-          ),
-        ),
-        Positioned(
-          top: size.height * 0.28,
-          left: size.width * 0.1,
-          child: InkWell(
-            onTap: () {
-              Navigator.of(context).pushNamed('/forgetpass');
-            },
+          Positioned.fill(
+            left: size.width * 0.35,
+            top: size.height * 0.03,
             child: Text(
-              "نسيت كلمة السر؟",
-              style: TextStyle(
-                color: Colors.blue[900],
+              "تسجيل دخول",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Positioned(
+            top: size.height * 0.08,
+            right: size.width * 0.1,
+            left: size.width * 0.1,
+            child: TextContainer(
+              labtext: "البريد الالكتروني",
+              onchange: () {},
+            ),
+          ),
+          Positioned(
+            top: size.height * 0.18,
+            right: size.width * 0.1,
+            left: size.width * 0.1,
+            child: TextContainer(
+              labtext: "كلمة السر",
+              onchange: () {},
+            ),
+          ),
+          Positioned(
+            top: size.height * 0.28,
+            left: size.width * 0.1,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/forgetpass');
+              },
+              child: Text(
+                "نسيت كلمة السر؟",
+                style: TextStyle(
+                  color: Colors.blue[900],
+                ),
               ),
             ),
           ),
+          Positioned(
+            top: size.height * 0.33,
+            left: size.width * 0.75,
+            child: InkWell(
+                onTap: () {}, child: SvgPicture.asset("assets/Button.svg")),
+          ),
+        ],
+      ),
+      SvgPicture.asset('assets/Mobile login-rafiki.svg'),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        SignInButton(
+          Buttons.Facebook,
+          shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          onPressed: () {},
         ),
-        Positioned(
-          top: size.height * 0.33,
-          left: size.width * 0.75,
-          child: InkWell(
-              onTap: () {}, child: SvgPicture.asset("assets/Button.svg")),
+      ]),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        SignInButton(
+          Buttons.Google,
+          shape: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          onPressed: () {
+            final provider =
+                Provider.of<GoogleSignInProvider>(context, listen: false);
+            provider.googlelogin();
+          },
         ),
-      ],
-    );
+      ])
+    ]);
   }
 }
