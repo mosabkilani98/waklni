7c2046d83a85c898c14b13df3fd48af2745f248f
yassin800 <57330560+yassin800@users.noreply.github.com>
2021-10-16 14:21:07 +0300

    bb

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:provider/provider.dart';
 import 'package:wakelni/provide/google.dart';
+=======
+import 'package:wakelni/network/diohelper.dart';
+
+>>>>>>> Stashed changes
 import 'package:wakelni/screens/changepass.dart';
 import 'package:wakelni/screens/checkcode.dart';
 import 'package:wakelni/screens/forgetpass.dart';
@@ -17,13 +22,17 @@ import 'package:wakelni/screens/services/marketing.dart';
 import 'package:wakelni/screens/services/programming.dart';
 import 'package:wakelni/screens/register.dart';
 import 'package:wakelni/screens/services/services.dart';
+<<<<<<< Updated upstream
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+=======
+>>>>>>> Stashed changes
 
 Future main () async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(Main());
+
 }
 
 class Main extends StatelessWidget {
@@ -48,7 +57,12 @@ class Main extends StatelessWidget {
           '/design': (context) => Design(),
           '/marketing': (context) => Marketing(),
           '/admins': (context) => Adminstration(),
+<<<<<<< Updated upstream
           '/order':(context)=>Order(),
         }));
+=======
+          '/order': (context) => Order(),
+        });
+>>>>>>> Stashed changes
   }
  
--- lib/screens/services/programming.dart
+++ lib/screens/services/programming.dart
@@ -1,185 +1,222 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wakelni/constants/constants.dart';
+import 'package:wakelni/network/diohelper.dart';
+import 'package:wakelni/screens/services/cubit/servicesstates.dart';
 import 'package:wakelni/widgets/ourbar.dart';
+
+import 'cubit/cubit.dart';
 
 class Program extends StatelessWidget {
   const Program({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    Response response;
     Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: PrimaryColor,
-        body: ListView(
-          children: [
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 10.0,
-                ),
-                IconButton(
-                    icon: Icon(
-                      Icons.menu,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return OurBar();
-                          });
-                    }),
-                SizedBox(
-                  width: size.width * 0.25,
-                ),
-                Image.asset(
-                  "assets/logo1.png",
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: size.width * 0.25,
-                ),
-                IconButton(
-                    icon: Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {}),
-              ],
-            ),
-            SizedBox(
-              height: size.height * 0.04,
-            ),
-            Container(
-              width: size.width,
-              height: size.height * 0.1,
-              color: Colors.white30,
-              child: Stack(
+    return BlocProvider(
+      create: (BuildContext context) => ServiceCubit(),
+      child: BlocConsumer<ServiceCubit, ServiceState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return SafeArea(
+            child: Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () async {
+                  try {
+                    var response = await Dio().get('https://192.168.0.70:3000/offer',
+                        options: Options(
+                          followRedirects: false,
+                        ));
+                    print(response);
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+              backgroundColor: PrimaryColor,
+              body: ListView(
                 children: [
-                  Center(
-                    child: Text(
-                      'برمجة',
-                      style: TextStyle(fontSize: 25.0, color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Directionality(
-              textDirection: TextDirection.rtl,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Wrap(
-                  children: [
-                    Text(
-                      'توفر لك هذه الخدمة',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                    Text(
-                      'احدث التقنيات',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                    Text(
-                      'احدث التقنيات',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                    Text(
-                      'لانشاء تطبيق خاص بك',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                    Text(
-                      'وعرض منتجاتك',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: size.height * 0.05,
-            ),
-            Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-              SizedBox(
-                width: size.width * 0.7,
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(25.0),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 10.0,
                       ),
-                      child: Column(
+                      IconButton(
+                          icon: Icon(
+                            Icons.menu,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return OurBar();
+                                });
+                          }),
+                      SizedBox(
+                        width: size.width * 0.25,
+                      ),
+                      Image.asset(
+                        "assets/logo1.png",
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: size.width * 0.25,
+                      ),
+                      IconButton(
+                          icon: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {}),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size.height * 0.04,
+                  ),
+                  Container(
+                    width: size.width,
+                    height: size.height * 0.1,
+                    color: Colors.white30,
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: Text(
+                            'برمجة',
+                            style:
+                                TextStyle(fontSize: 25.0, color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Wrap(
                         children: [
-                          Card(
-                            margin: EdgeInsets.all(8.0),
-                            color: Colors.white,
-                            child: Row(
-                              children: [
-                                Image.asset(
-                                    "assets/Website designer-amico.jpg"),
-                                Expanded(child: SizedBox()),
-                                Text('تطوير مواقع الويب'),
-                              ],
-                            ),
+                          Text(
+                            'توفر لك هذه الخدمة',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
                           ),
-                          Card(
-                            margin: EdgeInsets.all(8.0),
-                            color: Colors.white,
-                            child: Row(
-                              children: [
-                                Image.asset(
-                                    "assets/App development-rafiki.jpg"),
-                                Expanded(child: SizedBox()),
-                                Text('تطوير تطبيقات الجوال'),
-                              ],
-                            ),
+                          Text(
+                            'احدث التقنيات',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
                           ),
-                          Card(
-                            margin: EdgeInsets.all(8.0),
-                            clipBehavior: Clip.antiAlias,
-                            color: Colors.white,
-                            child: Row(
-                              children: [
-                                Image.asset(
-                                    "assets/Under construction-bro.jpg"),
-                                Expanded(child: SizedBox()),
-                                Text('صيانة المواقع والتطبيقات'),
-                              ],
-                            ),
+                          Text(
+                            'احدث التقنيات',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
                           ),
-                          SizedBox(
-                            height: 15.0,
-                          )
+                          Text(
+                            'لانشاء تطبيق خاص بك',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          Text(
+                            'وعرض منتجاتك',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
                         ],
                       ),
                     ),
-                    Positioned(
-                        left: size.width * 0.22,
-                        top: size.height * 0.3,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/order');
-                          },
-                          child: Text('طلب الخدمة'),
-                          style: ElevatedButton.styleFrom(
-                            primary: Color(0xff504595 - 0xffC21D4D),
-                            shape: CircleBorder(),
-                            padding: EdgeInsets.all(24),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.7,
+                          child: Stack(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Card(
+                                      margin: EdgeInsets.all(8.0),
+                                      color: Colors.white,
+                                      child: Row(
+                                        children: [
+                                          Image.asset(
+                                              "assets/Website designer-amico.jpg"),
+                                          Expanded(child: SizedBox()),
+                                          Text('تطوير مواقع الويب'),
+                                        ],
+                                      ),
+                                    ),
+                                    Card(
+                                      margin: EdgeInsets.all(8.0),
+                                      color: Colors.white,
+                                      child: Row(
+                                        children: [
+                                          Image.asset(
+                                              "assets/App development-rafiki.jpg"),
+                                          Expanded(child: SizedBox()),
+                                          Text('تطوير تطبيقات الجوال'),
+                                        ],
+                                      ),
+                                    ),
+                                    Card(
+                                      margin: EdgeInsets.all(8.0),
+                                      clipBehavior: Clip.antiAlias,
+                                      color: Colors.white,
+                                      child: Row(
+                                        children: [
+                                          Image.asset(
+                                              "assets/Under construction-bro.jpg"),
+                                          Expanded(child: SizedBox()),
+                                          Text('صيانة المواقع والتطبيقات'),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 15.0,
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Positioned(
+                                  left: size.width * 0.22,
+                                  top: size.height * 0.3,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pushNamed('/order');
+                                    },
+                                    child: Text('طلب الخدمة'),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Color(0xff504595 - 0xffC21D4D),
+                                      shape: CircleBorder(),
+                                      padding: EdgeInsets.all(24),
+                                    ),
+                                  )),
+                              SizedBox(
+                                height: 350.0,
+                              )
+                            ],
                           ),
-                        )),
-                    SizedBox(
-                      height: 350.0,
-                    )
-                  ],
-                ),
+                        ),
+                      ]),
+                ],
               ),
-            ]),
-          ],
-        ),
+            ),
+          );
+        },
       ),
     );
   }
